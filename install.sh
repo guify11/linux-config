a320e23370e32fea2af4050c21b3fb76fef9574e
Guilherme Silva Ferreira <guify.kmail@gmail.com>
2026-03-09 20:03:27 +0000

    adicionado o ficheiro sh para a config no linux

---
+++ install.sh
@@ -0,0 +1,5 @@
+printf "instalando:"
+printf "AdwSteamGtk, PrismLauncher, Flatseal, Steam, Brave Browser e Discord\n"
+
+
+flatpak install -s io.github.Foldex.AdwSteamGtk org.prismlauncher.PrismLauncher com.github.tchx84.Flatseal com.valvesoftware.Steam com.brave.Browser com.discordapp.Discord
